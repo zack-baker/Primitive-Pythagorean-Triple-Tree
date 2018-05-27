be36a940724e394d3b0c4ff3cecd2e7baa3f6a24
Zack <zack.baker@protonmail.ch>
2018-05-26 23:38:56 -0600

    Created a custom JButton class

---
+++ src/TreeButton.java
@@ -0,0 +1,49 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TreeButton extends JButton{
+
+	private Node btnNode;
+	private boolean wasClicked;
+	public TreeButton(){
+		super();
+		btnNode = new Node(0);
+		setText(btnNode.toString());
+		addActionListener(new Handler());
+		wasClicked = false;
+	}
+
+	public TreeButton(Node n){
+		super();
+		btnNode = n;
+		setText(btnNode.toString());	
+		addActionListener(new Handler());
+		wasClicked = false;
+	}
+
+	private class Handler implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event){
+			if(wasClicked==false){
+				setText("Clicked");
+				JPanel p = (JPanel)(((TreeButton)event.getSource()).getParent());
+				btnNode.generateChildren(0);
+				Node[] children = btnNode.getChildren();
+				for(int i=0;i<children.length;i++){
+					TreeButton b = new TreeButton(children[i]);
+					p.add(b);
+				}
+			}
+
+			wasClicked = true;
+
+			
+		}	
+	}
+}
+	
